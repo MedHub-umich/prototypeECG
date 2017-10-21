52cb79ab56d93e6f3d109509a219dda2494d2658
deedstho <deedstho@umich.edu>
2017-10-20 21:37:03 -0400

    changed Fs to about 341 Hz

--- prototypeECG.ino
+++ prototypeECG.ino
@@ -1,20 +1,26 @@
+// ADC
 int adcin    = A5;
 int adcvalue = 0;
 float mv_per_lsb = 3600.0F/1024.0F; // 10-bit ADC with 3.6V input range
+
+// GPIO - for timing
 
 void setup() {
   Serial.begin(115200);
 }
 
 void loop() {
+  // set timing gpio pin high
+  
+  
   // Get a fresh ADC value
   adcvalue = analogRead(adcin);
 
   // Display the results
-  Serial.print(adcvalue);
-  Serial.print(" [");
+  //Serial.print(adcvalue);
+  //Serial.print(" [");
   Serial.print((float)adcvalue * mv_per_lsb);
-  Serial.println(" mV]");
+  Serial.print('\n');
 
-  delay(10);
+  delay(2);
 }
